973dbc0f208cb84a0d37911ba3439492f189546c
Gaurav verma <mr.gaurav245@gmail.com>
2020-10-18 19:05:50 +0530

    Location checker is added

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // TODO (Check here whether GPS is ON or OFF using the method which we have created)
+        // START
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                    this,
+                    "Your location provider is turned off. Please turn it on.",
+                    Toast.LENGTH_SHORT
+            ).show()
+
+            // This will redirect you to settings from where you need to turn on the location provider.
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                    this,
+                    "Your location provider is already ON.",
+                    Toast.LENGTH_SHORT
+            ).show()
+        }
+        // END
+    }
+
+    /**
+     * A function which is used to verify that the location or GPS is enabled or not.
+     */
+    private fun isLocationEnabled(): Boolean {
+
+        // This provides access to the system location services.
+        val locationManager: LocationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+}
